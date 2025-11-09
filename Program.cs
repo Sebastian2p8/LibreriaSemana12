edd30422055dee270bbd231f00d0b954c209cf45
Arenas Medina Sebastián <neibenp21@gmail.com>
2025-11-08 23:42:38 -0500

    Subiendo desarrollo completo Semana12

--- Program.cs
+++ Program.cs
@@ -10,8 +10,51 @@ namespace Libreria
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Apellidos: Arenas Medina");
-            Console.WriteLine("Nombre: Sebastián");
+            Libreria libreria = new Libreria();
+            int opcion;
+
+            do
+            {
+                Console.WriteLine("\n===== MENÚ LIBRERÍA =====");
+                Console.WriteLine("1. Registrar libro ");
+                Console.WriteLine("2. Mostrar libros  ");
+                Console.WriteLine("3. Modificar libro ");
+                Console.WriteLine("4. Eliminar libro  ");
+                Console.WriteLine("5. Salir           ");
+                Console.Write("Seleccione una opción: ");
+
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out opcion))
+                {
+                    Console.WriteLine("Ingrese una opción válida ");
+                    continue;
+                }
+
+                switch (opcion)
+                {
+                    case 1:
+                        libreria.Registrar();
+                        break;
+                    case 2:
+                        libreria.Mostrar();
+                        break;
+                    case 3:
+                        libreria.Modificar();
+                        break;
+                    case 4:
+                        libreria.Eliminar();
+                        break;
+                    case 5:
+                        Console.WriteLine("Saliendo del programa ");
+                        break;
+                    default:
+                        Console.WriteLine("Opción inválida. Intente nuevamente ");
+                        break;
+                }
+
+
+            } while (opcion != 5);
         }
     }  
 }
