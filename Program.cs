af19f02e88f87184c032489ed10a1dcaf1c03716
Arenas Medina Sebastián <neibenp21@gmail.com>
2025-11-08 22:28:41 -0500

    Arenas Medina Sebastián

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Apellidos: Arenas Medina");
+            Console.WriteLine("Nombre: Sebastián");
+        }
+    }   
+}
